06e59db8aaa28d72aeff4da50e31687fa21ed2e8
Michele Pagnin <pagnin.michele@gmail.com>
2016-01-31 14:30:41 +0100

    Added support for Route::page($role, $classPath);

--- src/routing/Routing.php
+++ src/routing/Routing.php
@@ -11,21 +11,22 @@ final class Routing
     private static $putRouteList = array();
     private static $deleteRouteList = array();
     private static $allRouteList = array();
-    private static $customRouteList = array();
     private static $root = "";
 
-    public static function setRoot($root)
+    public static function setRoot( $root )
     {
         self::$root = $root;
     }
 
-    public static function add(Route $r)
+    public static function add( Route $r )
     {
-        if (MString::isNullOrEmpty($r->getType())) {
+        if( MString::isNullOrEmpty( $r->getType() ) )
+        {
             throw new EmptyRouteTypeException();
         }
 
-        switch ($r->getType()) {
+        switch( $r->getType() )
+        {
             case RouteType::GET:
                 self::$getRouteList[$r->getRole()] = $r;
                 break;
@@ -39,69 +40,79 @@ final class Routing
                 self::$deleteRouteList[$r->getRole()] = $r;
                 break;
             case RouteType::ALL:
+            case RouteType::PAGE:
                 self::$allRouteList[$r->getRole()] = $r;
-                break;
-            default:
-                self::$customRouteList[$r->getRole()] = $r;
                 break;
         }
     }
 
     public static function run()
     {
-        $requestType = strtolower($_SERVER['REQUEST_METHOD']);
-        $routeList = self::getRouteList($requestType);
+        $requestType = strtolower( $_SERVER['REQUEST_METHOD'] );
+        $routeList = self::getRouteList( $requestType );
         $role = self::getRole();
 
-        if (array_key_exists($role, $routeList) === false) {
-            throw new UndefinedRouteException($role);
+        if( array_key_exists( $role, $routeList ) === false )
+        {
+            throw new UndefinedRouteException( $role );
         }
 
         /* @var $route Route */
         $route = $routeList[$role];
         $className = $route->getClass();
         $methodName = $route->getMethod();
+        $classPath = $route->getClassPath();
 
-        if (!class_exists($className)) {
-            throw new ControllerNotExistsException($className);
+        if( $route->getType() === RouteType::PAGE )
+        {
+            require_once $classPath;
+            return;
+        }
+
+        if( !class_exists( $className ) )
+        {
+            throw new ControllerNotExistsException( $className );
         }
 
         $controller = new $className();
 
-        if ($controller instanceof MAbstractController) {
+        if( $controller instanceof MAbstractController )
+        {
             $controller->$methodName();
-        } else {
-            throw new InvalidControllerException($className);
+        }
+        else
+        {
+            throw new InvalidControllerException( $className );
         }
     }
 
     private static function getRole()
     {
-        $root = dirname($_SERVER['SCRIPT_NAME']);
+        $root = dirname( $_SERVER['SCRIPT_NAME'] );
         $role = $_SERVER['REQUEST_URI'];
 
-        if ($root != '/') {
-            $role = substr($role, strlen($root));
+        if( $root != '/' )
+        {
+            $role = substr( $role, strlen( $root ) );
         }
 
         return $role;
     }
 
-    private static function getRouteList($routeType)
+    private static function getRouteList( $routeType )
     {
-        switch ($routeType) {
+        switch( $routeType )
+        {
             case RouteType::GET:
-                return self::$getRouteList;
+                return array_merge( self::$getRouteList, self::$allRouteList );
             case RouteType::POST:
-                return self::$postRouteList;
+                return array_merge( self::$postRouteList, self::$allRouteList );
             case RouteType::PUT:
-                return self::$putRouteList;
+                return array_merge( self::$putRouteList, self::$allRouteList );
             case RouteType::DELETE:
-                return self::$deleteRouteList;
-            case RouteType::ALL:
-                return self::$allRouteList;
+                return array_merge( self::$deleteRouteList, self::$allRouteList );
             default:
-                return self::$customRouteList;
+                return array();
         }
     }
 }
